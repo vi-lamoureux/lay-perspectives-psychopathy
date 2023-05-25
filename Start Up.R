47cb3cba8de775c87ac0cf3496cd6a335ed2732f
vi-lamoureux <04-treads.envoys@icloud.com>
2023-05-25 10:59:25 -0500

    rstudio commit

---
+++ Start Up.R
@@ -0,0 +1,5 @@
+pacman::p_load(pacman, ggplot2, dplyr, plotly, rmarkdown, ggvis, tidyr, stringr, 
+               ggthemes, GGally, readxl, foreign, generics, highr, hms, knitr, 
+               prettyunits, purrr, tinytex, xtable, yaml, Hmisc, nnet, fmsb, 
+               lm.beta, fontspec, xcolor, rotating, xelatex, pdflatex, lualatex)
+
